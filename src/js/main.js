a4f14014db260b496269c6a33048f112ec03d619
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-20 15:47:12 +0200

    added event listener function for keydown

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,16 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+window.addEventListener('keydown', function (e) {
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+  if (!audio) return; // stop the function from running all together
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add('playing');
+})
 
-console.log(`Hello world!`);
+const keys = document.querySelectorAll('.key');
+
 
 
